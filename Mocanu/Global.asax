47aa7519012eb8b7fc63802256b3c6ffa6e9cf51
Sinoroth <denispredut@yahoo.com>
2018-11-26 21:46:05 +0200

    Original commit

---
+++ Mocanu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mocanu.MvcApplication" Language="C#" %>
---
+++ Mocanu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mocanu.Startup))]
+namespace Mocanu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
