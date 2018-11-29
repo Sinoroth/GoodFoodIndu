cc67eea11bc97420a7b69e8f3927ef1be78c16e9
Sinoroth <denispredut@yahoo.com>
2018-11-30 00:04:33 +0200

    Adding the client model properly

---
+++ Mocanu/Models/LModels/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mocanu.Models.LModels
+{
+    public class Client
+    {
+        [Key]
+        public string ClientId { get; set; }
+
+        public string Email { get; set; }
+
+        [MaxLength(50)]
+        public string Address { get; set; }
+
+        [MaxLength(2)]
+        [MinLength(2)]
+        public string ID_Card_Series { get; set; }
+
+        [MaxLength(6)]
+        [MinLength(6)]
+        public string ID_Card_Number { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string TelephoneNumber { get; set; }
+
+        public string Password { get; set; }
+    }
+}
