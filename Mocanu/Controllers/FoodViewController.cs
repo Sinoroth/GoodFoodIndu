553e6e2bfc022cc896cb63d27d26d66efa1640cd
Sinoroth <denispredut@yahoo.com>
2018-12-16 22:37:13 +0200

    Added a controller to the food view. Also added a data context for the database

---
+++ Mocanu/Controllers/FoodViewController.cs
@@ -0,0 +1,21 @@
+﻿using Mocanu.Data;
+using Mocanu.Models.LModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mocanu.Controllers
+{
+    public class FoodViewController : Controller
+    {
+        public ActionResult FoodPage()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+    }
+}
---
+++ Mocanu/Data/CateringContext.cs
@@ -0,0 +1,22 @@
+﻿using Mocanu.Models.LModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Mocanu.Data
+{
+    public class CateringContext : DbContext
+    {
+        public CateringContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Food> foods { get; set; }
+        public DbSet<Transaction> transactions { get; set; }
+
+        public System.Data.Entity.DbSet<Mocanu.Models.LModels.Client> Clients { get; set; }
+
+    }
+}
